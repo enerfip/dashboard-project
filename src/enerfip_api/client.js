26aeb66b750e08ec4c3a340f436167f423d9e1b9
bechir-elya <bechir.elya@gmail.com>
2025-05-09 14:41:11 +0200

    testing bas url without env

--- src/enerfip_api/client.js
+++ src/enerfip_api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = "https://redash-enerfip.herokuapp.com";
 
 export const getEnerfipQueryResult = async (queryId, apiKey) => {
   try {
